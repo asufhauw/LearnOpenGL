c78cefd268de3cfb92e335dfba3de010b98ace36
asufhauw <asufhauw@gmail.com>
2020-02-09 11:42:09 +0100

    Update gitignore and updated Main.cpp

--- LearnOpengl/Main.cpp
+++ LearnOpengl/Main.cpp
@@ -1,5 +1,15 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+const int HEIGHT = 600, WIDTH = 800;
+
+// GLFW call this function when user resizes the window
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+	std::cout << "Frame buffer size callback: (" << width << "," << height << ")";
+}
 
 int main()
 {
@@ -7,7 +17,39 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
+// IF MAC OS UNCOMMENT THE ROW BELLOW
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+// Create window
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+// Init GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+// Set opengl viewport, here is where opengl does it magic (-1,1) is scaled to (0,WIDTH),(0,HEIGHT)
+	glViewport(0, 0, WIDTH, HEIGHT);
+
+// register the callback functions after we've created the window and before the render loop is initiated.
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
+
