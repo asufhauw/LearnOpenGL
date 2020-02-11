c34080bc634e526a8120df13955b25a0dedb9404
asufhauw <asufhauw@gmail.com>
2020-02-11 22:14:13 +0100

    added new texture

--- LearnOpengl/Main.cpp
+++ LearnOpengl/Main.cpp
@@ -108,10 +108,13 @@ int main()
 
 // Textures
 	// Generate texture
-	unsigned int texture;
-	glGenTextures(1, &texture);
+	unsigned int texture1, texture2;
+	glGenTextures(1, &texture1);
+	glGenTextures(1, &texture2);
 	//bind it so any texture commands will configure the currently bound texture
-	glBindTexture(GL_TEXTURE_2D, texture);
+	glActiveTexture(GL_TEXTURE0); // activate the texture unit first before binding texture
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
 	// set wrapp to mirrored repeat
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
@@ -120,6 +123,7 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	// Load textures using stb_img.h
 	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
 	unsigned char* data = stbi_load("..\\Textures\\container.jpg", &width, &height, &nrChannels, 0);
 	if (data)
 	{
@@ -133,6 +137,32 @@ int main()
 	}
 	stbi_image_free(data);
 
+	glActiveTexture(GL_TEXTURE1); // activate the texture unit first before binding texture
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	// set wrapp to mirrored repeat
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+	//set mipmap mode
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	data = stbi_load("..\\Textures\\awesomeface.png", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		//Load image and Generate Mipmap 
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+	ourShader.use();
+	// either set it manually like so:
+	glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+	// or set it via the texture class
+	ourShader.setInt("texture2", 1);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -142,9 +172,13 @@ int main()
 		//render
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		glActiveTexture(GL_TEXTURE0); // activate the texture unit first before binding texture
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1); // activate the texture unit first before binding texture
+		glBindTexture(GL_TEXTURE_2D, texture2);
 
-		glBindTexture(GL_TEXTURE_2D, texture);
 		ourShader.use();
+		
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		//glDrawArrays(GL_TRIANGLES, 0, 3);
