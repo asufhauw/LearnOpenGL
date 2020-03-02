f2ad246b91a41ab3463cc62a5a1769c1b548efae
asufhauw <asufhauw@gmail.com>
2020-03-02 23:19:22 +0100

    made every 3'rd cube rotate

--- LearnOpengl/Main.cpp
+++ LearnOpengl/Main.cpp
@@ -209,6 +209,12 @@ int main()
 	  glm::vec3(1.5f,  0.2f, -1.5f),
 	  glm::vec3(-1.3f,  1.0f, -1.5f)
 	};
+
+	glm::mat4 projection;
+	projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+	int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
+	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 	while (!glfwWindowShouldClose(window))
 	{
 		// input
@@ -229,16 +235,13 @@ int main()
 		// note that we're translating the scene in the reverse direction of where we want to move
 		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
-		glm::mat4 projection;
-		projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
 		int modelLoc = glGetUniformLocation(ourShader.ID, "model");
 		int viewLoc = glGetUniformLocation(ourShader.ID, "view");
-		int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
 
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+	
 
 		ourShader.use();
 		
@@ -248,8 +251,9 @@ int main()
 			glm::mat4 model = glm::mat4(1.0f);
 			model = glm::translate(model, cubePositions[i]);
 			float angle = 20.0f * i;
-			//model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-			model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+			if (i % 3 == 0)  // every 3rd iteration (including the first) we set the angle using GLFW's time function.
+				angle = glfwGetTime() * 25.0f;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 			ourShader.setMat4("model", model);
 
 			glDrawArrays(GL_TRIANGLES, 0, 36);
